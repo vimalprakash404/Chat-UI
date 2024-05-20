8e2b0ec125a4c3e16475b41eacb9b219bd704b47
vimalprakash404 <vimalprakash332@gmail.com>
2024-05-20 23:38:50 +0530

    ui version 1

---
+++ src/App.jsx
@@ -0,0 +1,235 @@
+export default function App() {
+  return (
+    <div >
+      {/* HEADINGS */}
+      <div className="fixed w-full bg-green-400 h-16 pt-2 text-white flex shadow-md justify-between top-0">
+        {/* back button  */}
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24" 
+            className = "w-12 h-12 my-1 text-green-100 ml-2"
+          >
+            <path
+              className="text-gray-100 fill-current"
+              d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z"
+            />
+          </svg>
+        </a>
+        <div className="my-3 text-green-100 font-bold text-lg tracking-wide">Vimal prakash</div>
+
+        {/* 3 dots */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="w-8 h-8 mt-2 mr-2"
+        >
+          <path
+          className="text-green-100 fill-current"
+            fill-rule="evenodd"
+            d="M12 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+          />
+        </svg>
+      </div>
+      {/* message part  */}
+      <div className="mt-20 mb-10">
+        {/* single message 1 */}
+        <div >
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg">
+            This is basic mobile chat layout , built with tail wind css
+          </div>
+        </div>
+        {/* single message 2  */}
+
+
+        <div>
+          <div className="bg-green-200 w-3/4 mx-4 my-2 p-2 rounded-lg ml-auto">
+            it will be  used for ra full tutorial  about building a chat app Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime, tenetur molestiae quod magni pariatur neque nihil illo non odit harum architecto, accusantium provident labore voluptatem ab culpa praesentium eaque.
+          </div>
+        </div>
+
+        {/* single message 3 */}
+        <div>
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg "> 
+            check out my new server
+          </div>
+        </div>
+
+
+        {/* single message 1 */}
+        <div >
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg">
+            This is basic mobile chat layout , built with tail wind css
+          </div>
+        </div>
+        {/* single message 2  */}
+
+
+        <div>
+          <div className="bg-green-200 w-3/4 mx-4 my-2 p-2 rounded-lg ml-auto">
+            it will be  used for ra full tutorial  about building a chat app Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime, tenetur molestiae quod magni pariatur neque nihil illo non odit harum architecto, accusantium provident labore voluptatem ab culpa praesentium eaque.
+          </div>
+        </div>
+
+        {/* single message 3 */}
+        <div>
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg "> 
+            check out my new server
+          </div>
+        </div>
+
+
+
+        {/* single message 1 */}
+        <div >
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg">
+            This is basic mobile chat layout , built with tail wind css
+          </div>
+        </div>
+        {/* single message 2  */}
+
+
+        <div>
+          <div className="bg-green-200 w-3/4 mx-4 my-2 p-2 rounded-lg ml-auto">
+            it will be  used for ra full tutorial  about building a chat app Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime, tenetur molestiae quod magni pariatur neque nihil illo non odit harum architecto, accusantium provident labore voluptatem ab culpa praesentium eaque.
+          </div>
+        </div>
+
+        {/* single message 3 */}
+        <div>
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg "> 
+            check out my new server
+          </div>
+        </div>
+
+
+
+        {/* single message 1 */}
+        <div >
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg">
+            This is basic mobile chat layout , built with tail wind css
+          </div>
+        </div>
+        {/* single message 2  */}
+
+
+        <div>
+          <div className="bg-green-200 w-3/4 mx-4 my-2 p-2 rounded-lg ml-auto">
+            it will be  used for ra full tutorial  about building a chat app Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime, tenetur molestiae quod magni pariatur neque nihil illo non odit harum architecto, accusantium provident labore voluptatem ab culpa praesentium eaque.
+          </div>
+        </div>
+
+        {/* single message 3 */}
+        <div>
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg "> 
+            check out my new server
+          </div>
+        </div>
+
+
+
+
+        {/* single message 1 */}
+        <div >
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg">
+            This is basic mobile chat layout , built with tail wind css
+          </div>
+        </div>
+        {/* single message 2  */}
+
+
+        <div>
+          <div className="bg-green-200 w-3/4 mx-4 my-2 p-2 rounded-lg ml-auto">
+            it will be  used for ra full tutorial  about building a chat app Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime, tenetur molestiae quod magni pariatur neque nihil illo non odit harum architecto, accusantium provident labore voluptatem ab culpa praesentium eaque.
+          </div>
+        </div>
+
+        {/* single message 3 */}
+        <div>
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg "> 
+            check out my new server
+          </div>
+        </div>
+
+
+
+
+        {/* single message 1 */}
+        <div >
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg">
+            This is basic mobile chat layout , built with tail wind css
+          </div>
+        </div>
+        {/* single message 2  */}
+
+
+        <div>
+          <div className="bg-green-200 w-3/4 mx-4 my-2 p-2 rounded-lg ml-auto">
+            it will be  used for ra full tutorial  about building a chat app Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime, tenetur molestiae quod magni pariatur neque nihil illo non odit harum architecto, accusantium provident labore voluptatem ab culpa praesentium eaque.
+          </div>
+        </div>
+
+        {/* single message 3 */}
+        <div>
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg "> 
+            check out my new server
+          </div>
+        </div>
+
+
+
+
+        {/* single message 1 */}
+        <div >
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg">
+            This is basic mobile chat layout , built with tail wind css
+          </div>
+        </div>
+        {/* single message 2  */}
+
+
+        <div>
+          <div className="bg-green-200 w-3/4 mx-4 my-2 p-2 rounded-lg ml-auto">
+            it will be  used for ra full tutorial  about building a chat app Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maxime, tenetur molestiae quod magni pariatur neque nihil illo non odit harum architecto, accusantium provident labore voluptatem ab culpa praesentium eaque.
+          </div>
+        </div>
+
+        {/* single message 3 */}
+        <div>
+          <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg "> 
+            check out my new server
+          </div>
+        </div>
+      </div>
+    
+
+      {/* message input area */}
+
+      <div className="fixed bottom-0 w-full flex justify-between bg-green-100">
+        <textarea
+          rows="1"
+          placeholder="Message..."
+          className="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-300 resize-none"
+        ></textarea>
+        <button className="m-2">
+          <svg
+          className="svg-inline--fa text-green-400 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="paper-plane"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="currentColor"
+              d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"
+            />
+          </svg>
+        </button>
+      </div>
+
+    </div>
+  )
+}
